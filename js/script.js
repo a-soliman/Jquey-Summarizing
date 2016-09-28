3d23dfb0d17bd9500ad764e2d3a21874a3abdd1a
Ahmed Soliman <ahmedanwar@mail.com>
2016-09-28 09:00:59 -0700

    coded up the jquery to complate the project

--- js/script.js
+++ js/script.js
@@ -175,10 +175,35 @@ $(function() {
 
 	// checkbox.prop()
 
-	var textInput = $("input:text")
-	console.log(textInput.val())
-	textInput.val("Ahmed Soliman")
+	// var textInput = $("input:text")
+	// console.log(textInput.val())
+	// textInput.val("Ahmed Soliman")
 
-	var rangeInput = $("input[type='range']")
-	console.log(rangeInput.val())
+	// var rangeInput = $("input[type='range']")
+	// console.log(rangeInput.val())
+
+
+	//++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// Project photo Galarry 
+	//___________________
+
+	var galleryImage = $(".gallery").find("img").first()
+	var images = [
+		"images/laptop-mobile_small.jpg",
+		"images/laptop-on-table_small.jpg",
+		"images/people-office-group-team_small.jpg",
+		]
+
+		var i = 0
+		setInterval(function() {
+			i = (i + 1) % images.length // 0,1,2,0,1,... 
+			galleryImage.fadeOut(function() {
+				$(this).attr("src", images[i])
+				$(this).fadeIn()
+			})
+			console.log(galleryImage.attr("src"))
+		}, 2000)
+
 });
