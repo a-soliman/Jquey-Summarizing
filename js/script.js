9c0adde304e5ba96d92baf77745e0f53de7f5ab3
Ahmed Soliman <ahmedanwar@mail.com>
2016-09-27 21:18:13 -0700

    practicing Animation fadeIn fadeOut fade To fadeToggle slideDown slideUp hide and show()

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 $(function() {
 
-	
+	// $(".red-box").fadeOut(2000);
+	// $(".green-box").fadeOut(2000);
+	// $(".red-box").fadeIn(1000);
+	// $(".red-box").fadeTo(1000, 0.5);
+
+	// $(".red-box").fadeTo(3000, 0.2);
+	// $(".green-box").fadeTo(2000, 0.5);
+	// $(".blue-box").fadeTo(1000, 0.5);
+
+	// $(".green-box").fadeToggle();
+	// $(".green-box").fadeToggle();
+
+	// $(".green-box").fadeTo(1000, 0)
+
+	// $(".blue-box").hide();
+
+	// $(".blue-box").slideUp(500);
+	// $(".blue-box").slideDown()
+
+	// $("p").hide();
+	// $("p").slideDown(2000);
+
+	// $('.blue-box').slideToggle(2000)
 
 });
