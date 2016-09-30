09f60ddbdd3c7b35804c20835f96c97f28e4278c
Ahmed Soliman <ahmedanwar@mail.com>
2016-09-29 23:25:37 -0700

    practicing .on()

--- js/script.js
+++ js/script.js
@@ -330,21 +330,22 @@ $(function() {
 	// attaching the same elements to diffrent events
 	// .on('click', function() {.....})
 	//==================
-	var gallery = $(".gallery")
+	var galleryImage = $(".gallery").find("img")
+
+	galleryImage.on("click", changeImage)
+
+	function changeImage() {
 		var images = [
 		"images/laptop-mobile_small.jpg",
 		"images/laptop-on-table_small.jpg",
 		"images/people-office-group-team_small.jpg"
-	]
-	var i = 0 
-	gallery.find("img").on("click", function() {
+		]
+		var i = 0 
 		i = ( i + 1 ) % images.length
 	 	$(this).fadeOut(function() {
 	 		$(this).attr("src", images[i]).fadeIn()
 	 	})
-	})
-
-
+	}
 
 
 
