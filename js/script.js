a17f64a5f08bbb88eb51a4929de9f28050e585c5
Ahmed Soliman <ahmedanwar@mail.com>
2016-09-29 22:55:25 -0700

    attaching the same elements to diffrent events

--- js/script.js
+++ js/script.js
@@ -327,6 +327,28 @@ $(function() {
 
 
 
+	// attaching the same elements to diffrent events
+	// .on('click', function() {.....})
+	//==================
+	var gallery = $(".gallery")
+		var images = [
+		"images/laptop-mobile_small.jpg",
+		"images/laptop-on-table_small.jpg",
+		"images/people-office-group-team_small.jpg"
+	]
+	var i = 0 
+	gallery.find("img").on("click", function() {
+		i = ( i + 1 ) % images.length
+	 	$(this).fadeOut(function() {
+	 		$(this).attr("src", images[i]).fadeIn()
+	 	})
+	})
+
+
+
+
+
+
 
 
 
